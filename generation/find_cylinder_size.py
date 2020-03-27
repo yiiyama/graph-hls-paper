032f9dbfbb2d8dd2769b45d486fe8f127097bcb1
Yutaro <yutaro.iiyama@cern.ch>
2020-03-28 00:16:28 +0100

    plotting 95% quantile radius for each containment value

--- generation/find_cylinder_size.py
+++ generation/find_cylinder_size.py
@@ -5,17 +5,23 @@ ROOT.gROOT.SetBatch(True)
 import root_numpy as rnp
 import uproot
 
+plots_dir = '/tmp'
+
 geom_tree = uproot.open('geom.root')['detector']
 geom_data = geom_tree.arrays(['x', 'y', 'z'])
 
 coords = np.stack((geom_data['x'], geom_data['y'], geom_data['z']), axis=-1)
 
-data_blocks = uproot.iterate('/eos/cms/store/cmst3/user/yiiyama/graph_hls_paper/generated/123456/electron_10_100/events_22.root', 'events', ['genPid', 'genEnergy', 'genX', 'genY', 'recoEnergy'], entrysteps=128)
+#part = 'pioncharged'
+part = 'electron'
 
-hcont = ROOT.TH2D('containment', '', 101, 0., 1.01, 55, 4., 15.)
+data_blocks = uproot.iterate('/eos/cms/store/cmst3/user/yiiyama/graph_hls_paper/generated2/%s_10_100/123456/events_22.root' % part, 'events', ['genPid', 'genEnergy', 'genX', 'genY', 'recoEnergy'], entrysteps=128)
+
+matrix = ROOT.TH2D('matrix', '', 100, 0., 1., 55, 4., 15.)
+nent = 0
 
 for idata, data_block in enumerate(data_blocks):
-    for ient, (gen_p, gen_e, gen_x, gen_y, reco_e) in enumerate(zip(data_block['genPid'], data_block['genEnergy'], data_block['genX'], data_block['genY'], data_block['recoEnergy'])):
+    for gen_p, gen_e, gen_x, gen_y, reco_e in zip(*tuple(data_block[k] for k in ['genPid', 'genEnergy', 'genX', 'genY', 'recoEnergy'])):
         if gen_e == 0.:
             continue
         
@@ -24,17 +30,31 @@ for idata, data_block in enumerate(data_blocks):
         seed = np.tile(np.array([xseed, yseed]), (coords.shape[0], 1))
         dr2 = np.sum(np.square(coords[:, 0:2] - seed), axis=1)
 
+        c = 0.
         for r in np.arange(4., 15., 0.2, dtype=np.float):
             in_radius = np.asarray(dr2 < r * r).nonzero()
             total = np.sum(reco_e[in_radius]) * 1.e-3
             containment = total / gen_e
 
-            hcont.Fill(containment, r)
+            while c < containment:
+                matrix.Fill(c, r)
+                c += 0.01
 
-#    if idata == 10:
-#        break
+            if c >= 1.:
+                break
+
+        nent += 1
+
+arr = rnp.hist2array(matrix)
+quantile = np.cumsum(arr, axis=1) / nent
+
+graph = ROOT.TGraph(100)
+
+for ip in range(100):
+    q = np.searchsorted(quantile[ip], 0.95)
+    graph.SetPoint(ip, ip * 0.01, matrix.GetYaxis().GetBinCenter(q + 1))
 
 canvas = ROOT.TCanvas('c1', 'c1', 600, 600)
-hcont.Draw('COLZ')
-canvas.Print('/afs/cern.ch/user/y/yiiyama/www/plots/200110_cylinder/electron.pdf')
-canvas.Print('/afs/cern.ch/user/y/yiiyama/www/plots/200110_cylinder/electron.png')
+graph.Draw('APL')
+canvas.Print(plots_dir + '/%s_200cm.pdf' % part)
+canvas.Print(plots_dir + '/%s_200cm.png' % part)
